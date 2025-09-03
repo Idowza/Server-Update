1c007036da55d904951d714e2631eaf82b581573
Steven <steveniden@gmail.com>
2025-09-03 17:05:23 -0500

    removed root

--- update.sh
+++ update.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Check if the script is run as root
-if [ "$(id -u)" -ne 0 ]; then
-  echo "This script must be run as root. Please use sudo." >&2
-  exit 1
-fi
-
 # Colors
 GREEN='\033[0;32m'
 RED='\033[0;31m'
@@ -20,24 +14,24 @@ run_command() {
     echo # for a newline
 }
 
-run_command "Updating package list" "apt update -y"
-run_command "Upgrading all installed packages" "apt upgrade -y"
+run_command "Updating package list" "sudo apt update -y"
+run_command "Upgrading all installed packages" "sudo apt upgrade -y"
 
 # Update Flatpak packages if flatpak is installed
 if command -v flatpak &> /dev/null; then
-    run_command "Updating Flatpak packages" "flatpak update -y"
+    run_command "Updating Flatpak packages" "sudo flatpak update -y"
 else
     echo -e "${GREEN}Flatpak not found, skipping.${NC}\n"
 fi
 
-run_command "Cleaning up system" "apt autoremove --purge -y && apt autoclean -y"
-run_command "Fixing any broken dependencies" "apt install -f -y"
-run_command "Reconfiguring any packages that are not fully installed" "dpkg --configure -a"
+run_command "Cleaning up system" "sudo apt autoremove --purge -y && sudo apt autoclean -y"
+run_command "Fixing any broken dependencies" "sudo apt install -f -y"
+run_command "Reconfiguring any packages that are not fully installed" "sudo dpkg --configure -a"
 
 # Update Pi-hole if pihole is installed
 if command -v pihole &> /dev/null; then
-    run_command "Updating Pi-hole" "PIHOLE_SKIP_OS_CHECK=true pihole -up"
-    run_command "Updating Pi-hole's gravity list" "pihole -g"
+    run_command "Updating Pi-hole" "sudo PIHOLE_SKIP_OS_CHECK=true pihole -up"
+    run_command "Updating Pi-hole's gravity list" "sudo pihole -g"
 else
     echo -e "${GREEN}Pi-hole not found, skipping.${NC}\n"
 fi
